6186f8f8cf4ea236f38676ecb92e1eb33c484b5f
My Name <myEmail@example.com>
2019-10-30 16:22:39 +0700

    Реализуем первый тест в файле test_main_page

--- conftest.py
+++ conftest.py
@@ -1,11 +1,32 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: ru, en..")
 
 
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome(executable_path=r'c:\chromedriver\chromedriver.exe', language="en")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference("intl.accept_languages", user_language)
+
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(executable_path = 'C:/TEMP/DYAKOVA/Page-Object/geckodriver.exe')
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
+
